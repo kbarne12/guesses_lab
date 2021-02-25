32abccd5b44a25839969416a5c7dee2f9967befe
kbarne12 <kbarne12@gmail.com>
2021-02-25 16:50:30 -0500

    getGuess function2

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 const game = {
-    title:'Guess the number',
-     biggestNum: 100,
-     smallestNum: 1,
-     prevGuessess: [],
-     secretNum: null,
-     play: function() {
-        this.secretNum = Math.floor(Math.random() * 
-          (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
-          return this.secretNum
-
- }, 
-    getGuess: function(){ 
-        let guess: 
-        
-    while(isNan(guess) || guess<this.smallestNum || guess>this.biggestNum) {
-        guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
-
-     }
-
-
-}
-}
+    title: 'Guess the Number!',
+    biggestNum: 100,
+    smallestNum: 1,
+    secretNum: null,
+    prevGuesses: [],
+    getGuess: function() {
+      let guess;
+      while(isNaN(guess) || guess<this.smallestNum || guess>this.biggestNum)
+    {
+      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
+    }
+    },
+      play: function() {
+      this.secretNum = Math.floor(Math.random() * 
+        (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    }
+  }
+game.getGuess()
 
 
-  
-  game.getGuess()
